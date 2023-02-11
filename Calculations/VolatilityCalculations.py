72b9d5ff088dd17b11182e08a21c2b6657eeb6b0
Paul Epstein <pauldepstein@yahoo.com>
2023-02-11 19:28:19 +0000

    Tests for Black 76 pass.

--- Calculations/VolatilityCalculations.py
+++ Calculations/VolatilityCalculations.py
@@ -7,13 +7,6 @@ def historicalVol(df: pd.DataFrame, ticker: str, minSample: int = 5) -> float:
     A function for calculating annualized vols based
     on historical volatility as given by data
     in a dataframe.
-    A mature application would use exceptions.
-    However, this first draft assumes the data
-    is valid, trusting my visual inspection and
-    data cleaning.
-    For some easily determined errors, -1
-    is returned as a sentinel value, with a message
-    displayed.
     The dates are assumed to be sorted with values
     one business day apart. The volatility
     is calculated as the standard deviation
@@ -28,12 +21,10 @@ def historicalVol(df: pd.DataFrame, ticker: str, minSample: int = 5) -> float:
     """
 
     if ticker not in df.columns:
-        print("The dataframe does not correspond to your ticker")
-        return -1
+        raise ValueError("The data source does not correspond to your ticker")
 
     if len(df) < minSample:
-        print("Not enough data to determine volatility")
-        return -1
+        raise ValueError("Not enough data to determine volatility")
 
     DAYS_IN_YEAR = 260
     return np.nanstd(np.log(df[ticker]).diff()) * np.sqrt(DAYS_IN_YEAR)
