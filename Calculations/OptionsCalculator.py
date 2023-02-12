fc3b534bc12d7d4b9219924e8c23d58964e03129
Paul Epstein <pauldepstein@yahoo.com>
2023-02-12 21:37:33 +0000

    Change to directory structure

---
+++ Calculations/OptionsCalculator.py
@@ -0,0 +1,84 @@
+import numpy as np
+import scipy.stats as stats
+
+
+class OptionsCalculator:
+    """
+    Calculate option prices (currently implemented for Black76) and Greeks (later).
+    No error handling is done because the prevention of bad inputs
+    is regarded as the responsibility of the web interface.
+    """
+
+    def __init__(self, sigma: float, time: float, price: float, strike: float, rate: float = 0.05):
+        """
+        Option Parameters
+
+        Args:
+        sigma: annualized volatility
+        time:  time to expiry in years
+        price: current price of asset in USD
+        strike: strike in USD
+        rate:  US interest rate set to 5% for ease of computation.
+        Federal Reserve rate was actually 4.5% on February 8, 2023
+        """
+        self.sigma = sigma
+        self.time = time
+        self.price = price
+        self.strike = strike
+        self.rate = rate
+
+    def Black76(self, is_call: bool) -> float:
+        """A formula for pricing options on futures
+        Currently, the reference is https://en.wikipedia.org/wiki/Black_model
+        but a more authoritative reference can be given in future drafts
+
+        Arg:
+            is_call is True when option is a call and false for a put
+        Returns:
+            Present value of option
+
+        Using standard notation, the value of a call is exp(-rT) * (FN(d1) - KN(d2))
+        and the value of a put is exp(-rT) * (KN(-d2) - FN(-d1))
+        """
+        def call(_discount: float, _F: float, _K: float, _Nd1: float, _Nd2: float) -> float:
+            """
+            Nested functions for calls and puts compute the values assuming some intermediate values have
+            already been computed
+            :param _discount: This is exp(-rT) discounting the future payment to the present
+            :param _F: The value of the future
+            :param _K: The strike rate
+            :param _Nd1: A normcdf value expressed in standard notation
+            :param _Nd2: A normcdf value expressed in standard notation
+            :return: The fair value of the call option
+            """
+            return _discount * (_F * _Nd1 - _K * _Nd2)
+
+        def put(_discount: float, _F: float, _K: float, _Nd1: float, _Nd2: float) -> float:
+            """
+            The fair value for puts using the same parameters as in the case of calls.
+            Note that N(-x) = 1 - N(x)
+            """
+            Nminus_d1, Nminus_d2 = 1 - _Nd1, 1 - _Nd2
+            return discount * (_K * Nminus_d2 - _F * Nminus_d1)
+
+        def d1(_F: float, _K: float, sigma: float, time: float) -> float:
+            """
+            The d1 value, as in standard financial mathematics
+            is computed.  Parameter names are as previously stated.
+            """
+            ratio = _F / _K
+            logRatio = np.log(ratio)
+            addVol = logRatio + time * sigma * sigma / 2
+            return addVol/(sigma * np.sqrt(time))
+
+        # The value of the future is akin to the value of the asset
+        d1_value = d1(self.price, self.strike, self.sigma, self.time)
+        d2_value = d1_value - self.sigma * np.sqrt(self.time)
+        Nd1, Nd2 = stats.norm.cdf(d1_value), stats.norm.cdf(d2_value)
+
+        discount = np.exp(-self.rate * self.time)
+
+        # Now we have all the steps in place to just use the formula depending on whether we have
+        # a call or a put.
+        return call(discount, self.price, self.strike, Nd1, Nd2) if is_call else \
+            put(discount, self.price, self.strike, Nd1, Nd2)
---
+++ Calculations/PVCalculator.py
@@ -0,0 +1,66 @@
+from Calculations.OptionsCalculator import OptionsCalculator
+from Calculations.VolatilityCalculations import historicalVol
+from Calendar.CalendarComputations import timeBetween
+from Calendar.EnergyCalendar import EnergyCalendar
+import pandas as pd
+import numpy as np
+import datetime
+
+
+def PV(df: pd.DataFrame, ticker: str, monthsBack: int, strike: float, isCall: bool, rate: float = 0.05, monthsForward: int = 6 ) -> float:
+    """
+    Use the OptionsCalculator class together with the Calendar class to compute the PV of the options
+    :param: df is the DataFrame containing the price.  This is used for both the current value of the future and
+    the historical vol calculations.
+    :param: ticker is the ticker string
+    :param: monthsBack is the number of months before the delivery date that the option expires
+    :param: strike
+    :param: rate is the interest rate defaulted to 5% which is standard in simple applications
+    :param: monthsForward is the number of months forward from now that the delivery date occurs.
+    :param isCall is true for a call and false for a put
+    For example, if now is February 23, then a delivery date in April 23 would be two months forward.
+    To simplify early versions of the web form, user input is minimised and therefore a default value of
+    6 months forward is preset.
+    Standard quant notation is used for all parameters.
+    No formal testing (other than user observations) but the earlier functions have been unit-tested.
+    """
+    if not ticker in df.columns:
+        raise ValueError("Can not price -- inconsistent information")
+
+    if strike <= 0:
+        raise ValueError("Strike must be positive")
+
+    # Present value of future is final entry of dataframe
+    K = df[ticker].iloc[-1]
+    print("K =", K)
+    if K <= 0:
+        raise ValueError("Value of future must be positive")
+    # Sigma comes form historical volatilities
+    sigma = historicalVol(df, ticker)
+    print("The value of sigma is", sigma)
+    if sigma < 0 or np.isnan(sigma):
+        raise ValueError("Can not price -- error found in sigma")
+
+    # Use the calendar to find T -- the time to expiry
+    # First consider the beginning of the current month
+    today = datetime.datetime.now().date()
+    # EnergyCalendar is constructed via a tuple
+    todaytuple = tuple(today.timetuple())[0:3]
+    calendar = EnergyCalendar(todaytuple)
+    # For the expiry time, we shift monthsForward months
+    # forward when considering the delivery date and then
+    # monthsBack months back.
+    combinedShift = monthsForward - monthsBack
+    expiryDate = calendar.lastBusinessDayOfMonth(combinedShift)
+    # The expiry date is crucial information which should be
+    # given to the user.  A more sophisticated application
+    # would do something more than just a raw print statement.
+    print("The expiry date is", expiryDate)
+    T = timeBetween(today, expiryDate)
+    print("T is", T)
+    if T < 0:
+        raise ValueError("Error -- time to expiry can not be negative")
+
+    # Now all the elements are in place to price via the option calculator
+    option = OptionsCalculator(sigma, T, K, strike, rate)
+    return option.Black76(isCall)
